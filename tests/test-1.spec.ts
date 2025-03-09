9abb5a6ec7c23199823029c9f41fa1bcda0c7290
krishna2003 <m.krishna@faaztechsolutions.com>
2025-03-09 19:04:25 +0530

    3rd commit

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
+
   await page.goto('https://coinmarketcap.com/');
   await expect(page.getByRole('link', { name: 'Go to homepage' })).toBeVisible();
   await expect(page.getByRole('link', { name: 'Cryptocurrencies' })).toBeVisible();
@@ -10,4 +11,17 @@ test('test', async ({ page }) => {
   await expect(page.getByRole('link', { name: 'Exchanges' })).toBeVisible();
   const link2 = page.locator('role=link[name=Exchanges]');
   await link2.hover();
+  await expect(page.getByRole('link', { name: 'Spot' }).first()).toBeVisible();
+  await page.getByRole('link', { name: 'Spot' }).first().click();
+  await page.getByRole('button', { name: 'Derivatives', exact: true }).click();
+  await page.mouse.wheel(0, 6700);
+  // await expect(page.getByText('logo HyperPay Futures')).toBeVisible({ timeout: 20000 });
+  await page.getByRole('link', { name: 'logo HyperPay Futures' }).click();
+  // await expect(page.getByText('$3,830,766,357.61')).toBeVisible();
+  const page1Promise = page.waitForEvent('popup');
+  await page.getByRole('link', { name: 'https://www.hyperpay.io/' }).click();
+  const page1 = await page1Promise;
+  await page1.goto('https://www.hyperpay.io/futures/#/');
+  await page1.locator('.ad-close').click();
+  await expect(page1.getByText('登录').nth(1)).toBeVisible();
 });
